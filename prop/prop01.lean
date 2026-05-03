fbf3eb169abf3bb19ca57a1b84322b13f100279e
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2026-05-03 22:46:48 +0100

    Update prop01.lean

--- prop/prop01.lean
+++ prop/prop01.lean
@@ -1,6 +1,6 @@
 namespace prop01
-
+-- A ∧ (A → B) → B   :   modus ponens hidden behind a conjunction
 theorem prop01 : A ∧ (A → B) → B := by
-  sorry
-
+  intro ⟨hA, hAB⟩
+  exact hAB hA
 end prop01
