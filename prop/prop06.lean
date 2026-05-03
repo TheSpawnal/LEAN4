e7e7085180d527932ade935cc5e9e5d6fc46399d
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2026-05-03 22:48:48 +0100

    Update prop06.lean

--- prop/prop06.lean
+++ prop/prop06.lean
@@ -1,6 +1,13 @@
 namespace prop06
-
+-- A ∨ B ↔ B ∨ A   :   commutativity of disjunction, fully constructive.
 theorem prop06 : A ∨ B ↔ B ∨ A := by
-  sorry
-
+  constructor
+  · intro h
+    cases h with
+    | inl hA => exact Or.inr hA
+    | inr hB => exact Or.inl hB
+  · intro h
+    cases h with
+    | inl hB => exact Or.inr hB
+    | inr hA => exact Or.inl hA
 end prop06
